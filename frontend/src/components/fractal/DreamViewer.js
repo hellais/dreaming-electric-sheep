1ede8f938fe7a4c7f57bf6163034dd29c27d8194
Arturo Filastò <arturo@filasto.net>
2023-10-07 11:13:00 +0200

    Start sketching out fractal viewer
    Import flame generator from https://github.com/tariqksoliman/Fractal-Inferno/blob/master/scripts/FractalInferno.js

---
+++ frontend/src/components/fractal/DreamViewer.js
@@ -0,0 +1,12 @@
+export const DreamViewer = () => {
+  return (
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-12"></div>
+        <h3>Hello world</h3>
+      </div>
+    </div>
+  );
+};
+
+export default DreamViewer;
