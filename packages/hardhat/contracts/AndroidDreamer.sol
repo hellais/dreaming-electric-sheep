f34ede710ae210315321e759f0a44bc45b600b4c
Arturo Filastò <arturo@filasto.net>
2023-10-07 22:11:14 +0200

    Add final Android dreamer contract

--- packages/hardhat/contracts/AndroidDreamer.sol
+++ packages/hardhat/contracts/AndroidDreamer.sol
@@ -60,13 +60,10 @@ contract AndroidDreams is ERC721 {
 		uint8 entropyIdx
 	) internal returns (SD59x18) {
 		UD60x18 div = ud(uint8(entropy[entropyIdx]));
-		SD59x18 val;
 		if (div.eq(ud(0))) {
-			val = sd(1);
-		} else {
-			val = sd(1).div(div.intoSD59x18());
+			return sd(1);
 		}
-		return val;
+		return sd(1).div(div.intoSD59x18());
 	}
 
 	function _letAndroidDream() internal returns (Fractal[] memory) {
@@ -108,64 +105,60 @@ contract AndroidDreams is ERC721 {
 		bytes32 entropy = prng();
 		uint8 entropyIdx = 0;
 
-		uint8 numOfFuncs = 5; // XXX randomly generate number between 2-20
+		uint8 numOfFuncs = 3; // XXX randomly generate number between 2-20
 		Fractal[] memory fractalList = new Fractal[](numOfFuncs);
 		SD59x18 funcWeightSum = sd(0);
 		for (uint i = 0; i < numOfFuncs; i++) {
 			uint8 numOfVaris = 4; // randomize between 3-7
-			string[] memory v = new string[](numOfVaris);
-			SD59x18[] memory w = new SD59x18[](numOfVaris);
-			SD59x18[] memory col = new SD59x18[](3); //rgb triplet each 0 to 1
-			SD59x18[] memory c = new SD59x18[](6); //array of 6 coefficients
-			SD59x18 r; //Some random 0 to 1 var for possible use in variations
+			fractalList[i] = Fractal({
+				weight: sd(0),
+				col: new SD59x18[](3), //rgb triplet each 0 to 1,
+				c: new SD59x18[](6), //array of 6 coefficients,
+				w: new SD59x18[](numOfVaris),
+				v: new string[](numOfVaris)
+			});
+			//SD59x18 r; //Some random 0 to 1 var for possible use in variations
 			SD59x18 weightSum = sd(0);
 			for (uint j = 0; j < numOfVaris; j++) {
 				uint8 varIdx = uint8(entropy[entropyIdx]);
 				entropyIdx++;
 				varIdx = varIdx % uint8(VariationList.length);
-				v[j] = VariationList[varIdx];
-				w[j] = randomFixed(entropy, entropyIdx);
+				fractalList[i].v[j] = VariationList[varIdx];
+				fractalList[i].w[j] = randomFixed(entropy, entropyIdx);
 				entropyIdx++;
-				weightSum.add(w[j]);
+				weightSum.add(fractalList[i].w[j]);
 			}
 			// weights should sum to 1
-			for (uint i = 0; i < numOfVaris; i++) {
-				w[i] = w[i].div(weightSum);
+			for (uint j = 0; j < numOfVaris; j++) {
+				fractalList[i].w[j] = fractalList[i].w[j].div(weightSum);
 			}
 			//col
-			col[0] = randomFixed(entropy, entropyIdx);
+			fractalList[i].col[0] = randomFixed(entropy, entropyIdx);
 			entropyIdx++;
-			col[1] = randomFixed(entropy, entropyIdx);
+			fractalList[i].col[1] = randomFixed(entropy, entropyIdx);
 			entropyIdx++;
-			col[2] = randomFixed(entropy, entropyIdx);
+			fractalList[i].col[2] = randomFixed(entropy, entropyIdx);
 			entropyIdx++;
 
 			//c
 			for (uint j = 0; j < 6; j++) {
-				c[j] = randomFixed(entropy, entropyIdx).mul(sd(3)).sub(
-					sd(1.5e18)
-				);
+				fractalList[i].c[j] = randomFixed(entropy, entropyIdx)
+					.mul(sd(3))
+					.sub(sd(1.5e18));
 				entropyIdx++;
 			}
 
 			//r
-			r = randomFixed(entropy, entropyIdx);
+			//r = randomFixed(entropy, entropyIdx);
+			entropyIdx++;
+			fractalList[i].weight = randomFixed(entropy, entropyIdx);
 			entropyIdx++;
 
-			SD59x18 funcWeight = randomFixed(entropy, entropyIdx);
-			entropyIdx++;
-			fractalList[i] = Fractal({
-				weight: funcWeight,
-				col: col,
-				c: c,
-				w: w,
-				v: v
-			});
-			funcWeightSum.add(funcWeight);
+			funcWeightSum.add(fractalList[i].weight);
 		}
 		// funcweights should sum to 1
-		for (uint i = 0; i < numOfFuncs; i++) {
-			fractalList[i].weight = fractalList[i].weight.div(funcWeightSum);
+		for (uint j = 0; j < numOfFuncs; j++) {
+			fractalList[j].weight = fractalList[j].weight.div(funcWeightSum);
 		}
 		return fractalList;
 	}
